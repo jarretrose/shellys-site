91391d5c3d1a80af3a3da819c85a3a90a382ee71
Jarret Rose <jarretrose@gmail.com>
2019-06-20 19:42:06 -0400

    getting basic express app up and running

--- server/app.js
+++ server/app.js
@@ -1,1 +1,8 @@
 // express config
+
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,1 +1,6 @@
 // port config
+
+const app = require('./app');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`App is listening on port ${port}`));

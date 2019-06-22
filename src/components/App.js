69f3cfe2c1ed6f5552bb6d155cd62a7efd75b830
Jarret Rose <jarretrose@gmail.com>
2019-06-22 00:54:16 -0400

    nav & appbar styling, background image

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,24 @@ import Nav from './Nav.js'
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-    margin: '0 0',
-    backgroundImage: `url("../../images/girl-in-pink.jpg")`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center center',
-    width: '100vw',
-    height: '100vh'
+    margin: '0',
+    backgroundColor: '#000',
+    [theme.breakpoints.down('sm')]: {
+      backgroundImage: `url("../../images/girl-in-pink-small.png")`,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center right',
+      width: '100vw',
+      height: '100vh',
+    },
+    [theme.breakpoints.up('md')]: {
+      backgroundImage: `url("../../images/girl-in-pink-test.png")`,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center left',
+      width: '100vw',
+      height: '100vh',
+    },
   },
 }));
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
+// import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
+// import InboxIcon from '@material-ui/icons/MoveToInbox';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import MailIcon from '@material-ui/icons/Mail';
+// import MailIcon from '@material-ui/icons/Mail';
 import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 const drawerWidth = 160;
-const drawerHeight = 400;
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -32,9 +31,8 @@ const useStyles = makeStyles(theme => ({
   },
   appBar: {
     marginLeft: drawerWidth,
-    [theme.breakpoints.up('sm')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-    },
+    backgroundColor: '#d1939d',
+    zIndex: theme.zIndex.drawer + 1,
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -45,6 +43,7 @@ const useStyles = makeStyles(theme => ({
   toolbar: theme.mixins.toolbar,
   drawerPaper: {
     width: drawerWidth,
+    backgroundColor: '#d1939d',
   },
   content: {
     flexGrow: 1,

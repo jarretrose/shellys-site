e31722e800b6aee330bf0e6af83b48dc9bd7cd02
Jarret Rose <jarretrose@gmail.com>
2019-08-04 12:43:27 -0400

    typo

--- server/app.js
+++ server/app.js
@@ -31,13 +31,11 @@ app.use('/api', api);
 
 // custom error handler
 app.use(function(err, req, res, next) {
-
   res.status(err.status || 500);
   res.json({
     message: err.message,
     error: req.app.get('env') === 'development' ? err : {},
   });
-
 });
 
 

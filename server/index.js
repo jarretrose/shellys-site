aa8521958f2f96392ebec77af6e8e2c5462c8158
Jarret Rose <jarretrose@gmail.com>
2019-08-15 14:58:17 -0400

    removing seed data

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@
 
 const app = require('./app');
 const port = process.env.PORT || 8080;
-const init = require('./models/syncAndSeed')
+// const init = require('./models/syncAndSeed')
 
 app.listen(port, () => console.log(`App is listening on port ${port}.`));
 
-init();
+// init();

6da387702fc6087c25047d07f55bda259cd4e272
Jarret Rose <jarretrose@gmail.com>
2019-08-15 10:58:29 -0400

    some modal editing

--- src/components/ImageModal.js
+++ src/components/ImageModal.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { showModalAction, hideModalAction } from '../store'
-import { withStyles, makeStyles, useTheme } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 // const styles = {
-  const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(theme => ({
   modal: {
     position: 'fixed',
     zIndex: 3000,
     paddingTop: 100,
     [theme.breakpoints.down('sm')]: {
-      paddingTop: 0,
+      paddingTop: 50,
     },
     left: 0,
     top: 0,
@@ -28,7 +28,9 @@ import Typography from '@material-ui/core/Typography';
     maxHeight: '90%',
   },
   button: {
-    padding: 0
+    padding: 0,
+    height: 25,
+    width: 50
   },
   buttonText: {
     fontFamily: "'Satisfy', cursive",
@@ -41,13 +43,16 @@ import Typography from '@material-ui/core/Typography';
     fontSize: 'calc(16px + 12 * ((100vw - 320px) / 960))',
     color: 'rgb(255,255,255)',
     textAlign: 'center',
+  },
+  imageContainer: {
+    position: 'relative'
   }
 }))
 
 const closeButton = {
   position: 'absolute',
-  bottom: '50px',
-  right: '35px',
+  bottom: -30,
+  right: 55,
   cursor: 'pointer'
 }
 
@@ -64,15 +69,20 @@ const ImageModal = (props) => {
   // Material-UI's Dialog and Modal are nice... but not easily customized. I was wrestling with getting the modal to display larger and cleanly on mobile devices and it just wasn't cooperating because of all the internals. I went with a simple CSS solution instead and got more or less exactly what I wanted. 
   return (
     <div className={classes.modal}>
+
       <Typography className={classes.prettyText}>
         {props.name}
       </Typography>
-      <img src={props.imageURL} className={classes.modalImage} />
-      <span style={closeButton}>
-        <Button className={classes.button} variant="contained" color="secondary" onClick={handleClose}>
-          <Typography className={classes.buttonText}>Close</Typography>
-        </Button>
-      </span>
+
+      <div className={classes.imageContainer}>
+        <img src={props.imageURL} className={classes.modalImage} />
+        <div style={closeButton}>
+          <Button className={classes.button} variant="contained" color="secondary" onClick={handleClose}>
+            <Typography className={classes.buttonText}>Close</Typography>
+          </Button>
+        </div>
+      </div>
+
     </div>
   )
 }

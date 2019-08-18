3182afe50a70e31b9686a84056b313ed65e25e8a
Jarret Rose <jarretrose@gmail.com>
2019-08-18 19:32:22 -0400

    image modal

--- src/components/ImageModal.js
+++ src/components/ImageModal.js
@@ -7,39 +7,33 @@ import Fab from '@material-ui/core/Fab'
 import CloseIcon from '@material-ui/icons/Close'
 
 const useStyles = makeStyles(theme => ({
-  modal: {
+  root: {
     display: 'flex',
     justifyContent: 'center',
-    alignContent: 'center',
+    alignItems: 'center',
     position: 'fixed',
     zIndex: 3000,
     paddingTop: 0,
-    [theme.breakpoints.down('sm')]: {
-      paddingTop: 50,
-      flexDirection: 'column'
-    },
     left: 0,
     top: 0,
     width: '100%',
     height: '100%',
     overflow: 'auto',
-    backgroundColor: 'rgba(0,0,0,.75)'
+    backgroundColor: 'rgba(0,0,0,.75)',
+    [theme.breakpoints.down('sm')]: {
+      paddingTop: 40,
+      flexDirection: 'column'
+    },
   },
   modalImage: {
     margin: 'auto',
-    maxWidth: '95%',
-    maxHeight: '95%',
-    [theme.breakpoints.down('sm')]: {
-      maxWidth: '100%',
-      maxHeight: '100%',
-    },
+    padding: 10,
+    maxWidth: '100%',
+    maxHeight: '100%',
   },
-  imageText: {
+  description: {
     margin: 'auto',
-    maxWidth: '50%',
-    [theme.breakpoints.down('sm')]: {
-      maxWidth: '100%'
-    },
+    padding: 10,
   },
   button: {
     padding: 0,
@@ -57,11 +51,13 @@ const useStyles = makeStyles(theme => ({
   prettyText: {
     fontFamily: "'Satisfy', cursive",
     textTransform: 'none',
-    fontSize: 32,
+    fontSize: 24,
     color: 'rgb(255,255,255)',
-    textAlign: 'center',
-    padding: 10,
+    textAlign: 'left',
   },
+  prettText2: {
+    fontSize: 100,
+  }
 }))
 
 const closeButton = {
@@ -81,19 +77,22 @@ const ImageModal = (props) => {
 
   const handleClose = () => closeModal()
 
-  // Material-UI's Dialog and Modal are nice... but not easily customized. I was wrestling with getting the modal to display larger and cleanly on mobile devices and it just wasn't cooperating because of all the internals. I went with a simple CSS solution instead and got more or less exactly what I wanted. 
+  // Material-UI's Dialog and Modal are nice... but not easily customized. I was wrestling with getting the modal to display larger and cleanly on mobile devices and it just wasn't cooperating because of all the internals. I went with a CSS solution instead and got more or less exactly what I wanted. 
   return (
-    <div className={classes.modal} onClick={handleClose}>
-      <img src={props.imageURL} className={classes.modalImage} />
-      <div className={classes.imageText}>
-        <Typography className={classes.prettyText}>{props.name}</Typography>
-        <Typography className={classes.prettyText}>{props.desc}</Typography>
-      </div>
-      <div style={closeButton}>
-        <Fab size='small' color="secondary" className={classes.fab}>
-          <CloseIcon />
-        </Fab>
-      </div>
+    <div className={classes.root} onClick={handleClose}>
+
+        <img src={props.imageURL} className={classes.modalImage} />
+
+        <div className={classes.description}>
+          <Typography className={classes.prettyText}>{props.name}</Typography>
+          <Typography className={classes.prettyText}>{props.desc}</Typography>
+        </div>
+        <div style={closeButton}>
+          <Fab size='small' color="secondary" className={classes.fab}>
+            <CloseIcon />
+          </Fab>
+        </div>
+
     </div>
   )
 }
